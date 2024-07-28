7111d544a5eb2a6947597ae5267c2c92e958f720
Luis Diaz <v-lureynoso@expediagroup.com>
2024-07-28 14:32:55 -0700

    Do not send error data back to client

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -2,11 +2,11 @@ const errorHandler = (error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    const data = error.data;
+    // const data = error.data; // Do not send back the whole error to the client
     res.status(status).json({
         success: false,
         errorMessage: message,
-        data: data
+        data: null
     });
 };
 
