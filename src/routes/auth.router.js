986dd401961247169a8bd78a3121865a19022bca
Luis Diaz <v-lureynoso@expediagroup.com>
2024-08-10 14:43:51 -0700

    Testing prettier

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,18 +1,24 @@
-const router = require('express').Router();
-const AuthController = require('#controllers/auth.controller');
-const {registerValidator, updateStatusValidator} = require('#validators/auth.validator');
-const {isAuthenticated} = require('#middleware/authentication.middleware');
+const router = require("express").Router();
+const AuthController = require("#controllers/auth.controller");
+const {
+  registerValidator,
+  updateStatusValidator,
+} = require("#validators/auth.validator");
+const { isAuthenticated } = require("#middleware/authentication.middleware");
 
 // Register new user account
-router.post('/register', registerValidator, AuthController.register);
-
+router.post("/register", registerValidator, AuthController.register);
 // Log in user
-router.post('/login', AuthController.login);
-
+router.post("/login", AuthController.login);
 // Get user status
-router.post('/status', isAuthenticated, AuthController.getUserStatus);
+router.post("/status", isAuthenticated, AuthController.getUserStatus);
 
 // Update user status
-router.patch('/status', isAuthenticated, updateStatusValidator, AuthController.updateUserStatus);
+router.patch(
+  "/status",
+  isAuthenticated,
+  updateStatusValidator,
+  AuthController.updateUserStatus,
+);
 
 module.exports = router;
