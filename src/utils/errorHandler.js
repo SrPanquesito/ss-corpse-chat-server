c0936a5c1909800f06a6e22b4120c5b8a22b7131
Luis Diaz <v-lureynoso@expediagroup.com>
2024-08-11 12:05:13 -0700

    Testing prettier config

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,13 +1,13 @@
 const errorHandler = (error, req, res) => {
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message || "An error occurred";
-  // const data = error.data; // Do not send back the whole error to the client
-  res.status(status).json({
-    success: false,
-    errorMessage: message,
-    data: null,
-  });
-};
+    console.log(error)
+    const status = error.statusCode || 500
+    const message = error.message || 'An error occurred'
+    // const data = error.data; // Do not send back the whole error to the client
+    res.status(status).json({
+        success: false,
+        errorMessage: message,
+        data: null,
+    })
+}
 
-module.exports = errorHandler;
+module.exports = errorHandler
