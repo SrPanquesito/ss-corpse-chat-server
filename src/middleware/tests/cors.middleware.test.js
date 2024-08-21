76a7ef6f31895cdca1ec6a816bd5428033ddb727
Luis Diaz <v-lureynoso@expediagroup.com>
2024-08-21 16:20:50 -0700

    Updated unit test for cors middleware

--- src/middleware/tests/cors.middleware.test.js
+++ src/middleware/tests/cors.middleware.test.js
@@ -1,7 +1,35 @@
-const { corsOptions } = require("../cors.middleware");
+describe('src/middleware/cors.middleware.js', () => {
+    let originalEnv
 
-describe("src/middleware/cors.middleware.js", () => {
-  test("corsOptions is defined", () => {
-    expect(corsOptions).toBeDefined();
-  });
-});
+    afterAll(() => {
+        // Restore the original environment variables
+        process.env = originalEnv
+    })
+
+    beforeAll(() => {
+        // Save the original environment variables
+        originalEnv = process.env
+    })
+
+    afterEach(() => {
+        // Clear the require cache to reset the module state
+        jest.resetModules()
+    })
+
+    test('corsOptions is defined', () => {
+        process.env = { ...originalEnv }
+        const { corsOptions } = require('../cors.middleware')
+        expect(corsOptions).toBeDefined()
+    })
+
+    test('should log an error and use an empty allowlist if ALLOW_LIST is invalid JSON', () => {
+        console.error = jest.fn() // Mock console.error
+        process.env = { ...originalEnv, ALLOW_LIST: 'invalid-json' }
+        const { corsOptions } = require('../cors.middleware')
+        expect(console.error).toHaveBeenCalledWith(
+            'Failed to parse ALLOW_LIST:',
+            expect.any(SyntaxError)
+        )
+        expect(corsOptions.origin).toEqual([])
+    })
+})
