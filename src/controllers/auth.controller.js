1723bd0ed9a26b2fdbb7a8f1ed22445b2be45c42
Luis Diaz <v-lureynoso@expediagroup.com>
2024-09-22 02:39:15 -0700

    Add prefix to profilePicture on register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,105 +1,111 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const {
-  errorsOnValidation,
-} = require("#controllers/utils/validationResultChecker");
-const Users = require("#models/users.model");
-const { uploadFileToS3 } = require("#clients/aws.s3.client");
+    errorsOnValidation,
+} = require('#controllers/utils/validationResultChecker')
+const Users = require('#models/users.model')
+const { uploadFileToS3 } = require('#clients/aws.s3.client')
 
 const register = async (req, res, next) => {
-  if (errorsOnValidation(req, res, next)) return;
+    if (errorsOnValidation(req, res, next)) return
 
-  try {
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const profilePicture = req.file || null;
-    const s3File = profilePicture ? await uploadFileToS3(profilePicture) : null;
-    const user = await Users.create({
-      email,
-      username,
-      password: hashedPassword,
-      status: "ACTIVE",
-      profilePictureUrl: s3File?.Location || "",
-    });
+    try {
+        const email = req.body.email
+        const username = req.body.username
+        const password = req.body.password
+        const hashedPassword = await bcrypt.hash(password, 12)
 
-    const token = jwt.sign(
-      {
-        userId: user.id,
-        email: user.email,
-        username: user.username,
-        profilePictureUrl: user.profilePictureUrl,
-        status: user.status,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "6h" },
-    );
+        let s3File
+        if (req.file) {
+            req.file.originalname = 'profilePicture-' + req.file.originalname
+            s3File = await uploadFileToS3(req.file)
+        }
+        const user = await Users.create({
+            email,
+            username,
+            password: hashedPassword,
+            status: 'ACTIVE',
+            profilePictureUrl: s3File?.Location || '',
+        })
 
-    res.status(201).json({
-      success: true,
-      errorMessage: null,
-      data: {
-        id: user.id,
-        email: user.email,
-        token,
-      },
-    });
-  } catch (error) {
-    if (!error.statusCode) {
-      error.statusCode = 500;
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                email: user.email,
+                username: user.username,
+                profilePictureUrl: user.profilePictureUrl,
+                status: user.status,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '6h' }
+        )
+
+        res.status(201).json({
+            success: true,
+            errorMessage: null,
+            data: {
+                id: user.id,
+                email: user.email,
+                token,
+            },
+        })
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500
+        }
+        next(error)
     }
-    next(error);
-  }
-};
+}
 
 const login = async (req, res, next) => {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
-    const user = await Users.findOne({ where: { email } });
-    if (!user) {
-      const error = new Error("A user with this email could not be found.");
-      error.statusCode = 401;
-      throw error;
-    }
+    try {
+        const email = req.body.email
+        const password = req.body.password
+        const user = await Users.findOne({ where: { email } })
+        if (!user) {
+            const error = new Error(
+                'A user with this email could not be found.'
+            )
+            error.statusCode = 401
+            throw error
+        }
 
-    const isEqual = await bcrypt.compare(password, user.password);
-    if (!isEqual) {
-      const error = new Error("Wrong password!");
-      error.statusCode = 401;
-      throw error;
-    }
-    const token = jwt.sign(
-      {
-        userId: user.id,
-        email: user.email,
-        username: user.username,
-        profilePictureUrl: user.profilePictureUrl,
-        status: user.status,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "6h" },
-    );
+        const isEqual = await bcrypt.compare(password, user.password)
+        if (!isEqual) {
+            const error = new Error('Wrong password!')
+            error.statusCode = 401
+            throw error
+        }
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                email: user.email,
+                username: user.username,
+                profilePictureUrl: user.profilePictureUrl,
+                status: user.status,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '6h' }
+        )
 
-    res.status(200).json({
-      success: true,
-      errorMessage: null,
-      data: {
-        id: user.id,
-        email: user.email,
-        token,
-      },
-    });
-  } catch (error) {
-    if (!error.statusCode) {
-      error.statusCode = 500;
+        res.status(200).json({
+            success: true,
+            errorMessage: null,
+            data: {
+                id: user.id,
+                email: user.email,
+                token,
+            },
+        })
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500
+        }
+        next(error)
     }
-    next(error);
-  }
-};
+}
 
 module.exports = {
-  register,
-  login,
-};
+    register,
+    login,
+}
