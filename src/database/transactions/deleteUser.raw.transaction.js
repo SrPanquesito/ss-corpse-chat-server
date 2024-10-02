abb26c57cafc8a2ebdffcb03dbbff670ca5aa0db
Luis Diaz <v-lureynoso@expediagroup.com>
2024-10-02 00:34:47 -0700

    Saving stored procedure in script

--- src/database/transactions/deleteUser.raw.transaction.js
+++ src/database/transactions/deleteUser.raw.transaction.js
@@ -28,3 +28,29 @@ async function execute(userId) {
 
 // Enter UUID here
 execute('d69c9c9b-257d-4f12-9ede-40a78fa4686a')
+
+/*
+-- MySQL Stored Procedure
+
+DELIMITER //
+CREATE PROCEDURE DeleteUserById(IN inputUserId VARCHAR(255))
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- Rollback the transaction in case of an error
+        ROLLBACK;
+    END;
+    -- Start a new transaction
+    START TRANSACTION;
+    -- Delete from UserVerification
+    DELETE FROM UserVerification WHERE userId = inputUserId;
+    -- Delete from UserSettings
+    DELETE FROM UserSettings WHERE userId = inputUserId;
+    -- Delete from Users
+    DELETE FROM Users WHERE id = userId;
+    -- Commit the transactiona
+    COMMIT;
+END //
+DELIMITER ;
+
+*/
